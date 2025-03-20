6b974427c5b7de2542d22e8544c2d9e6cf51d7bb
unknown <sebastien33goulet@gmail.com>
2025-03-19 22:21:22 -0400

    test

--- queryOllama.js
+++ queryOllama.js
@@ -1,17 +1,11 @@
-//const OllamaApiModel = require('./ollamaApiModelService');
-
-//const OLLAMA_URL = "http://localhost";
-//const OLLAMA_PORT = 11434;
-//const MODEL = "llama3.2:3b"; // Change selon le modèle dispo
-//const PROMPT_INTRO = "Analyse et explique les logs trouvés dans ces fichiers Java :\n\n";
-
-const fs = require('fs');
-const axios = require('axios');
-const { execSync } = require("child_process");
+import fs from "fs";
+import axios from "axios";
+import { execSync } from "child_process";
+import { Octokit } from "@octokit/rest";
 
 const url_lama = 'http://127.0.0.1:8000/improve-logs';
 const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
-const { Octokit } = await import("@octokit/rest");
+
 const octokit = new Octokit({ auth: GITHUB_TOKEN });
 const PR_NUMBER = process.env.PR_NUMBER;
 const REPO = process.env.REPO;
